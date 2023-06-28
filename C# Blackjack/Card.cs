543d34ae8442a3168c41622039155425c8b87320
nonam00 <raisky.ilya3@yandex.ru>
2023-06-29 00:31:13 +0300

    Переписан из C++ кода

---
+++ C# Blackjack/Card.cs
@@ -0,0 +1,45 @@
+﻿namespace C__Blackjack
+{
+    internal class Card //entity of the card
+    {
+        public static List<string> pack = new(); //колода карт
+        private static int pack_count = 52; //счётчик количества карт в колоде 
+
+        private string name; //имя карты
+        private int mark; //значение карты в очках
+        public static void PackInit() //инициализация колоды карт
+        {
+            for (int i = 2; i <= 10; i++)
+                for (int j = 0; j < 4; j++)
+                    pack.Add(i.ToString());
+            for (int i = 0; i < 4; i++)
+            {
+                pack.Add("J");
+                pack.Add("Q");
+                pack.Add("K");
+                pack.Add("A");
+            }
+        }
+        public Card()
+        {
+            Random rnd = new();
+
+            name = pack[rnd.Next(0, pack_count--)];
+            pack.Remove(name);
+
+            if (name == "J" || name == "Q" || name == "K")
+                mark = 10;
+            else if (name == "A")
+                mark = 11;
+            else
+                mark = Convert.ToInt32(name);
+        }
+        public string Name
+        { get { return name; } }
+        public int Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+    }
+}
---
+++ C# Blackjack/Hand.cs
@@ -0,0 +1,110 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace C__Blackjack
+{
+    internal class Hand
+    {
+        private List<Card> cards = new();
+        private int score;
+        //distribution of cards to the player
+        public Hand()
+        {
+            score = 0;
+            for (int i = 0; i < 2; i++)
+                this.AddCard();
+        }
+
+        //prints player cards
+        public void Print(int type)
+        {
+            if (type == 0)
+                Console.WriteLine("Your cards:\n");
+            else
+                Console.WriteLine("Dealer's cards:\n");
+            for (int i = 0; i < cards.Count; i++)
+                Console.Write("##############\t");
+            Console.WriteLine();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < cards.Count(); j++)
+                {
+                    if (!(type == 1 && j == 1))
+                    {
+                        if (i == 1)
+                        {
+                            Console.Write($"#  {cards[j].Name}");
+                            if (cards[j].Name == "10")
+                                Console.Write("        #\t");
+                            else
+                                Console.Write("         #\t");
+                            continue;
+                        }
+                        else if (i == 5)
+                        {
+                            Console.Write($"#         {cards[j].Name}");
+                            if (cards[j].Name == "10")
+                                Console.Write(" #\t");
+                            else
+                                Console.Write("  #\t");
+                            continue;
+                        }
+                    }
+                    if (i == 7)
+                        Console.Write("##############\t");
+                    else
+                        Console.Write("#            #\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //adds a card to the person
+        public void AddCard()
+        {
+            Card temp = new();
+            score += temp.Mark;
+            cards.Add(temp);
+        }
+
+        //do a split
+        public void Split()
+        {
+            score -= cards[cards.Count - 1].Mark;
+            cards.RemoveAt(cards.Count - 1);
+            this.AddCard();
+        }
+
+        //ñhanges the value of card A to 1 if it is in hand
+        public void BustCancel()
+        {
+            foreach(Card item in cards)
+                if (item.Name == "A" && item.Mark == 11)
+                {
+                    item.Mark = 1;
+                    score -= 10;
+                    break;
+                }
+        }
+
+        //checking for the ability to split
+        public bool SplitCheck() => cards.Count == 2 && cards[0].Mark == cards[1].Mark;
+
+        //checks about score
+        public bool auto_win()
+        {
+            if ((cards[0].Name == "A" && cards[1].Name == "A") || score == 21)
+                return true;
+            else
+                return false;
+        }
+
+        //bust check
+        public bool Bust() => score > 21;
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+    }
+}
